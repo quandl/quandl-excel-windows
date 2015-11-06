b01f232675ac97d44f11dbeb66d6a909ea395044
Clement Leung <clement@quandl.com>
2015-11-06 12:36:17 -0500

    Persist Application and User Settings Login feature Settings panel feature

---
+++ QuandlExcelAddinWindows/Controls/QuandlSettings.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Quandl.Shared;
+
+namespace Quandl.Excel.Addin.Controls
+{
+    public partial class QuandlSettings : UserControl
+    {
+        public delegate void AuthTokenChanged();
+        public event AuthTokenChanged SettingsAuthTokenChanged;
+        public QuandlSettings()
+        {
+            InitializeComponent();
+            apiKeyTextBox.Text = QuandlConfig.ApiKey;
+            autoUpdate.Checked = QuandlConfig.AutoUpdate;
+        }
+
+        private void autoUpdate_CheckedChanged(object sender, EventArgs e)
+        {
+        }
+
+        protected virtual void OnSettingsAuthTokenChanged()
+        {
+            SettingsAuthTokenChanged?.Invoke();
+        }
+
+        private void saveSettings_Click(object sender, EventArgs e)
+        {
+            QuandlConfig.AutoUpdate = autoUpdate.Checked;
+            QuandlConfig.ApiKey = apiKeyTextBox.Text;
+            OnSettingsAuthTokenChanged();
+        }
+
+        public void UpdateApiKeyTextBox()
+        {
+            apiKeyTextBox.Text = QuandlConfig.ApiKey;
+        }
+    }
+}
---
+++ QuandlShared/QuandlConfig.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quandl.Shared
+{
+    public class QuandlConfig
+    {
+        public static string ApiKey
+        {
+            get { return Properties.Settings.Default.ApiKey; }
+            set
+            {
+                Properties.Settings.Default.ApiKey = value;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        public static bool AutoUpdate
+        {
+            get { return Properties.Settings.Default.AutoUpdate; }
+            set
+            {
+                Properties.Settings.Default.AutoUpdate = value;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        public static void Reset()
+        {
+            Properties.Settings.Default.Reset();
+        }
+    }
+}
