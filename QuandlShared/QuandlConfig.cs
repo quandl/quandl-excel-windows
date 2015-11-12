a27e72dd28788b25259995b24290ef04b5363e3a
Clement Leung <clement@quandl.com>
2015-11-12 16:04:00 -0500

    use registry

--- QuandlShared/QuandlConfig.cs
+++ QuandlShared/QuandlConfig.cs
@@ -3,34 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Win32;
 
 namespace Quandl.Shared
 {
     public class QuandlConfig
     {
+        private const string RegistrySubKey = @"SOFTWARE\Quandl\ExcelAddin";
+
         public static string ApiKey
         {
-            get { return Properties.Settings.Default.ApiKey; }
-            set
-            {
-                Properties.Settings.Default.ApiKey = value;
-                Properties.Settings.Default.Save();
-            }
+            get { return GetRegistry<string>("ApiKey"); }
+            set { SetRegistryKeyValue("ApiKey", value); }
         }
 
         public static bool AutoUpdate
         {
-            get { return Properties.Settings.Default.AutoUpdate; }
-            set
-            {
-                Properties.Settings.Default.AutoUpdate = value;
-                Properties.Settings.Default.Save();
-            }
+            get { return Convert.ToBoolean(GetRegistry<int>("AutoUpdate")); }
+            set { SetRegistryKeyValue("AutoUpdate", Convert.ToInt32(value), RegistryValueKind.DWord); }
         }
 
         public static void Reset()
         {
-            Properties.Settings.Default.Reset();
+            Registry.CurrentUser.DeleteSubKeyTree(RegistrySubKey);
+        }
+
+        private static void SetRegistryKeyValue(string key, object value, RegistryValueKind regValueKing = RegistryValueKind.String)
+        {
+            var appKeyPath = Microsoft.Win32.Registry.CurrentUser.CreateSubKey(RegistrySubKey);
+            var apiSubKey = appKeyPath.CreateSubKey(key);
+            apiSubKey.SetValue(key, value, regValueKing);
+            apiSubKey.Close();
+        }
+
+        private static T GetRegistry<T>(string key)
+        {
+            var quandlRootKey = Microsoft.Win32.Registry.CurrentUser.OpenSubKey(RegistrySubKey);
+            if (quandlRootKey != null)
+            {
+                var subKey = quandlRootKey.OpenSubKey(key);
+                if (subKey != null)
+                {
+                    return (T)subKey.GetValue(key, default(T));
+                }
+            }
+
+            return default(T);
         }
     }
 }
