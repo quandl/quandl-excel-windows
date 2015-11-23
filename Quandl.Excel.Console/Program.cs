35629aeeabebcc025d8b40c170849cf3798073a0
Jun Li <junlix@yahoo.com>
2015-11-23 10:58:04 -0500

    setup project updates and other fixes

---
+++ Quandl.Excel.Console/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quandl.Shared;
+
+
+namespace Quandl.Excel.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length >= 1 && args[0].Equals("r"))
+            {
+                ExcelHelp.RegisterExcelAddin();
+            }
+        }
+    }
+}
