1c3f1c863a0ef5b5f50530fcd49af5ed8d46bfdf
Clement Leung <clement@quandl.com>
2016-06-24 13:51:13 -0400

    switch to httpclient and models for api requests

---
+++ QuandlShared/models/DatabaseCollection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quandl.Shared.models
+{
+    public class DatabaseCollection
+    {
+        public List<Database> Databases { get; set; }
+        public Meta Meta { get; set; }
+    }
+}
