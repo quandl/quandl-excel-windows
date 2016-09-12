62c2e375f284b091c1cf33e608aeb0efb12e6505
Michael Fich <michael@michaelfich.com>
2016-09-12 11:19:18 -0400

    Coded UI Tests for Step 3 of the UDF Builder (#68)

--- Quandl.Shared.Modules/models/DataColumn.cs
+++ Quandl.Shared.Modules/models/DataColumn.cs
@@ -6,18 +6,15 @@ namespace Quandl.Shared.Models
     public class DataColumn : DependencyObject
     {
         public string Name { get; set; }
-
         public string Code => Name.ToUpper();
-
         public DataHolderDefinition Parent { get; set; }
-
         public ProviderType ProviderType { get; set; }
-
         public string Type { get; set; }
-
         public string LongName => ParentProperty != null ? $"{ParentProperty.Name} - {Name}" : Name;
-
-        private DataHolderDefinition ParentProperty => (DataHolderDefinition) GetValue(CheckedItemHelper.ParentProperty)
-            ;
+        private DataHolderDefinition ParentProperty => (DataHolderDefinition) GetValue(CheckedItemHelper.ParentProperty);
+        public override string ToString()
+        {
+            return LongName;
+        }
     }
 }
