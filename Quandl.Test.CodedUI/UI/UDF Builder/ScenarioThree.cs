96691c9f8bc7d8197c7e1b8b93d5f8098b537c9f
izzat <eib6@yahoo.com>
2016-09-23 16:26:54 -0400

    Add Scenario two and three

--- Quandl.Test.CodedUI/UI/UDF Builder/ScenarioThree.cs
+++ Quandl.Test.CodedUI/UI/UDF Builder/ScenarioThree.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Quandl.Shared.Models;
 using Quandl.Test.CodedUI.Helpers;
+using System.Collections.Generic;
 
 namespace Quandl.Test.CodedUI.UI.UDF_Builder
 {
@@ -9,21 +10,22 @@ namespace Quandl.Test.CodedUI.UI.UDF_Builder
     /// Scenario one in the Excel Test Case Sheet
     /// </summary
     ///<remarks>
-    ///Page 1 - Database(Step 1) :Enter Wiki in search.
+    ///Page 1 - Database(Step 1): look for wiki using browse.
     ///Page 2 - Data(step 2): Search for FMC Corp. (FMC) Prices, Dividends, Splits and Trading Volume and select it.
-    ///Page 3 - Columns(step 3): Don't select columns.
-    ///Page 4 - Filters(step 4): Select these filteres: All historical , Transformation: row-on-row %change.
-    ///Page 5 - Placement(step 5): include headers.
-    ///Verify UDF signature: =QSERIES(\"WIKI/FMC\",,,,\"rdiff\")
+    ///Page 3 - Columns(step 3): Select the following columns Date Open High Low Close Ex-Dividend.
+    ///Page 4 - Filters(step 4): Select these filteres: from date 1-1-2016 to 1-2-2016 , Transformation: row-on-row change, frequency monthly.
+    ///Page 5 - Placement(step 5): not to include headers.
+    ///Verify UDF signature: =QSERIES({\"WIKI/FMC/DATE\",\"WIKI/FMC/OPEN\",\"WIKI/FMC/HIGH\",\"WIKI/FMC/LOW\",\"WIKI/FMC/CLOSE\",\"WIKI/FMC/EX-DIVIDEND\"},{\"2016-01-01\",\"2016-02-01\"},\"monthly\",,\"diff\",,False))
     ///Click insert.
     ///</remarks>
 
     [CodedUITest]
-    public class ScenarioOne
+    public class ScenarioThree
     {
         public UIMap UIMap => map ?? (map = new UIMap());
         private UIMap map;
         private Dataset _dataset;
+        private static List<DataColumn> _datasetColumns;
 
         #region Additional test attributes
 
@@ -45,16 +47,18 @@ namespace Quandl.Test.CodedUI.UI.UDF_Builder
 
         #endregion  
         [TestMethod]
-        public void TestCases1()
+        public void TestCases3()
         {
+            var browseOptions = CodedUITestHelpers.selectStockUsMfOne();
             _dataset = CodedUITestHelpers.FreeDataset();
-            var filters = CodedUITestHelpers.filtersAllHistRdiff();
-            var expectedUDF = "=QSERIES(\"WIKI/FMC\",,,,\"rdiff\")";
-            CodedUITestHelpers.CompleteStep1("WIKI");
+            _datasetColumns = CodedUITestHelpers.DateOpenHighLowCloseExDividendDatasetColumns();
+            var filters = CodedUITestHelpers.filtersDateRangetMonthlyDiff();
+            var expectedUDF = "=QSERIES({\"WIKI/FMC/DATE\",\"WIKI/FMC/OPEN\",\"WIKI/FMC/HIGH\",\"WIKI/FMC/LOW\",\"WIKI/FMC/CLOSE\",\"WIKI/FMC/EX-DIVIDEND\"},{\"2016-01-01\",\"2016-02-01\"},\"monthly\",,\"diff\",,False)";
+            CodedUITestHelpers.CompleteBrowseStep1(browseOptions);
             CodedUITestHelpers.CompleteStep2(_dataset, _dataset.Name);
-            CodedUITestHelpers.CompleteStep3(null);
+            CodedUITestHelpers.CompleteStep3(_datasetColumns);
             CodedUITestHelpers.CompleteStep4(filters);
-            CodedUITestHelpers.CompleteStep5(false);
+            CodedUITestHelpers.CompleteStep5(true);
             UIMap.AssertCorrectUDFSignature(expectedUDF);
             UIMap.ClickInsertButton();
         }
