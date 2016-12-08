8e57fbed7030d8f38845edec550fb449839d2ecd
Jun Li <jun@quandl.com>
2016-12-08 18:38:22 -0500

    one installer

--- Quandl.Excel.Console/Program.cs
+++ Quandl.Excel.Console/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Quandl.Excel.Console
 {
@@ -8,19 +9,43 @@ namespace Quandl.Excel.Console
         // RegisterExcelAddIn function will handle the case when the Quandl excel add-in registry key created before install
         private static void Main(string[] args)
         {
-            if (args.Length == 0) return;
-            if (args[0].Equals("register"))
+            try
             {
-                SetupHelp.RegisterExcelAddin();
-                return;
+                if (args.Length == 0) return;
+                if (args[0].Equals("register"))
+                {
+                    SetupHelp.RegisterExcelAddin();
+                    return;
+                }
+                if (args[0].Equals("unregister"))
+                {
+                    SetupHelp.UnRegisterExcelAddin();
+                    return;
+                }
             }
-            if (args[0].Equals("unregister"))
+            catch (Exception e)
             {
-                SetupHelp.UnRegisterExcelAddin();
-                return;
+                using (StreamWriter w = File.AppendText(GetTempPath() + @".\quandl_excel_install_error_log.txt"))
+                {
+                    Log(e.Message, w);
+                }
             }
+            
 
             throw new Exception("Could not identify command to use. Please use either `register` or `unregister`.");
+        }
+        private static void Log(string logMessage, TextWriter w)
+        {
+            w.Write("\r\nLog Entry : ");
+            w.WriteLine("{0} {1}", DateTime.Now.ToLongTimeString(), DateTime.Now.ToLongDateString());
+            w.WriteLine("  :{0}", logMessage);
+            w.WriteLine("-------------------------------");
+        }
+        private static string GetTempPath()
+        {
+            string path = System.Environment.GetEnvironmentVariable("TEMP");
+            if (!path.EndsWith("\\")) path += "\\";
+            return path;
         }
     }
 }
---
+++ Quandl.Excel.Console/Version.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace Quandl.Excel.Console
+{
+    public class Version
+    {
+        private static string AddinPackageString = "Quandl.Excel.UDF.Functions-AddIn.xll";
+        private static string BaseValue = $@"{Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)}\Quandl";
+        private string ValueFor32 = $@"/R ""{BaseValue}\32bit\{AddinPackageString}""";
+        private string ValueFor64 = $@"/R ""{BaseValue}\64bit\{AddinPackageString}""";
+        private string ExcelExecutable = "excel.exe";
+        private string[] ExcelInstallFolders = new string[8]
+        {
+            @"C:\Program Files (x86)\Microsoft Office\Office{0}\",
+            @"C:\Program Files (x86)\Microsoft Office\root\Office{0}\",
+            @"C:\Program Files (x86)\Microsoft Office {0}\Office{0}\",
+            @"C:\Program Files (x86)\Microsoft Office {0}\root\Office{0}\",
+            @"C:\Program Files\Microsoft Office {0}\Office{0}\",
+            @"C:\Program Files\Microsoft Office {0}\root\Office{0}\",
+            @"C:\Program Files\Microsoft Office\Office{0}\",
+            @"C:\Program Files\Microsoft Office\root\Office{0}\"
+
+        };
+        internal string _excelVersion = null;
+
+        public Version(string excelVersion)
+        {
+            _excelVersion = excelVersion;
+        }
+
+        public string AddinPath
+        {
+            get
+            {
+                string value;
+                if (Is64BitOperatingSystem())
+                {
+                    value = IsExcel32BitInstall(_excelVersion) ? ValueFor32 : ValueFor64;
+
+                }
+                else
+                {
+                    value = ValueFor32;
+                }
+                return value;
+            }
+        }
+
+        private bool IsExcel32BitInstall(string version)
+        {
+
+            string folder = null;
+            string file = string.Empty;
+            foreach (var f in ExcelInstallFolders)
+            {
+                folder = string.Format(f, version.Replace(".0", ""));
+                file = FindExistingExcelFromFolder(folder);
+                if (file != string.Empty)
+                    break;
+            }
+
+            if (file != string.Empty)
+            {
+                return Introspection.GetPEFormat(file) == Introspection.PEFormat.PE32;
+            }
+
+
+            return false;
+        }
+
+
+        private string FindExistingExcelFromFolder(string folder)
+        {
+            string result = string.Empty;
+
+            if (Directory.Exists(folder))
+            {
+                var file = Directory.EnumerateFiles(folder).ToArray().FirstOrDefault(x => x.ToLower().EndsWith(ExcelExecutable));
+                if (file != null)
+                    result = file;
+            }
+            
+            return result;
+        }
+
+        #region Is64BitOperatingSystem (IsWow64Process)
+        // http://1code.codeplex.com/SourceControl/changeset/view/39074#842775
+        internal static bool Is64BitOperatingSystem()
+        {
+            if (IntPtr.Size == 8)  // 64-bit programs run only on Win64
+            {
+                return true;
+            }
+            else  // 32-bit programs run on both 32-bit and 64-bit Windows
+            {
+                // Detect whether the current process is a 32-bit process 
+                // running on a 64-bit system.
+                bool flag;
+                return ((DoesWin32MethodExist("kernel32.dll", "IsWow64Process") &&
+                    IsWow64Process(GetCurrentProcess(), out flag)) && flag);
+            }
+        }
+
+        static bool DoesWin32MethodExist(string moduleName, string methodName)
+        {
+            IntPtr moduleHandle = GetModuleHandle(moduleName);
+            if (moduleHandle == IntPtr.Zero)
+            {
+                return false;
+            }
+            return (GetProcAddress(moduleHandle, methodName) != IntPtr.Zero);
+        }
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetCurrentProcess();
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+        static extern IntPtr GetModuleHandle(string moduleName);
+
+        [DllImport("kernel32", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern IntPtr GetProcAddress(IntPtr hModule,
+            [MarshalAs(UnmanagedType.LPStr)]string procName);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool IsWow64Process(IntPtr hProcess, out bool wow64Process);
+
+        #endregion
+    }
+}
