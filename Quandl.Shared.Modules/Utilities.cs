469bf2bb7932d99153f0609060d1e05cdf3b6a29
Matthew Basset <matthew@quandl.com>
2017-06-07 10:56:09 -0400

    Version bump to 3.85.0

--- Quandl.Shared.Modules/Utilities.cs
+++ Quandl.Shared.Modules/Utilities.cs
@@ -9,7 +9,7 @@ namespace Quandl.Shared
 {
     public class Utilities
     {
-        public const string ReleaseVersion = "v3.84.0";
+        public const string ReleaseVersion = "v3.85.0";
         public const string ReleaseSource = "excel";
         public const string DateFormat = "yyyy-MM-dd";
         public enum UserRoles
