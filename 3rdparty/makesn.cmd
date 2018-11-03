31d3db2c9050e4cbc8375587e890ada86766e420
Stan Ostashewski <info@osmtech.biz>
2018-11-02 20:41:02 -0600

    fixed signing assemblies

--- 3rdparty/makesn.cmd
+++ 3rdparty/makesn.cmd
@@ -1,11 +1,10 @@
-rem sn -k keyPair.snk
+"%ProgramFiles(x86)%\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6 Tools\sn.exe" -k keyPair.snk
 set PATH_ILDASM="%ProgramFiles(x86)%\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6 Tools\ildasm.exe"
 set PATH_ILASM=%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\ilasm.exe
-set sourcepath=..\packages\Octokit.0.24.0\lib\net45\octokit.dll
 call :runsn ..\lib\Markdown.Xaml.dll Markdown.Xaml
+call :runsn ..\packages\Octokit.0.24.0\lib\net45\octokit.dll octokit
 call :runsn ..\packages\SharpRaven.2.2.0\lib\net45\SharpRaven.dll SharpRaven
 call :runsn ..\packages\Syroot.Windows.IO.KnownFolders.1.0.2\lib\net40\Syroot.Windows.IO.KnownFolders.dll Syroot.Windows.IO.KnownFolders
-
 EXIT /B %ERRORLEVEL%
 
 :runsn
