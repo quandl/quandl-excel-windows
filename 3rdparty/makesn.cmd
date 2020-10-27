8c415c691f06dc4fd35a35db73f9a225fc903420
Jamie Couture <jamie@quandl.com>
2020-10-27 17:40:09 -0400

    3rdparty: copy redistributables
    Update makesn.cmd to also copy needed files from Add-in redistributables
    to simplify setup process.
    
    Signed-off-by: Jamie Couture <jamie@quandl.com>

--- 3rdparty/makesn.cmd
+++ 3rdparty/makesn.cmd
@@ -1,10 +1,13 @@
 "%ProgramFiles(x86)%\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6 Tools\sn.exe" -k keyPair.snk
 set PATH_ILDASM="%ProgramFiles(x86)%\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6 Tools\ildasm.exe"
 set PATH_ILASM=%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\ilasm.exe
+set PATH_AIE="%ProgramFiles(x86)%\Add-in Express\Add-in Express for .NET\Redistributables"
 call :runsn ..\lib\Markdown.Xaml.dll Markdown.Xaml
 call :runsn ..\packages\Octokit.0.24.0\lib\net45\Octokit.dll octokit
 call :runsn ..\packages\SharpRaven.2.2.0\lib\net45\SharpRaven.dll SharpRaven
 call :runsn ..\packages\Syroot.Windows.IO.KnownFolders.1.0.2\lib\net40\Syroot.Windows.IO.KnownFolders.dll Syroot.Windows.IO.KnownFolders
+call :cpdll ..\Quandl.Excel.Addin\Loader
+call :cpexe ..\Quandl.Excel.AddinSetup\4.0.0\Resources
 EXIT /B %ERRORLEVEL%
 
 :runsn
@@ -13,4 +16,12 @@ del %2%.*
 %PATH_ILASM% %2%.il /dll /key=keyPair.snk
 del %2%.il
 del %2%.res
+
+:cpdll
+copy %PATH_AIE%\adxloader64.dll %1%
+copy %PATH_AIE%\adxloader.dll %1%
+EXIT /B 0
+
+:cpexe
+copy %PATH_AIE%\vdwtool.exe %1%
 EXIT /B 0
