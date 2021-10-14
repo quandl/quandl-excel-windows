d3de2cf9aff5218b777891617bb33d7a858a61df
Jamie Couture <jamie@quandl.com>
2021-10-14 09:55:50 -0400

    excel-windows 3.96.0
    Signed-off-by: Jamie Couture <jamie@quandl.com>

--- Quandl.Shared.Modules/Utilities.cs
+++ Quandl.Shared.Modules/Utilities.cs
@@ -9,7 +9,7 @@ namespace Quandl.Shared
 {
     public class Utilities
     {
-        public const string ReleaseVersion = "v3.95.5";
+        public const string ReleaseVersion = "v3.96.0";
         public const string ReleaseSource = "excel";
         public const string DateFormat = "yyyy-MM-dd";
         public enum UserRoles
